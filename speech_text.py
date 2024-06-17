2b4ded9dc03c56cf78338e3800602431ce8ff8ad
ashwingeeni12 <99517865+ashwingeeni12@users.noreply.github.com>
2024-06-17 16:59:00 -0500

    Update speech_text.py

--- speech_text.py
+++ speech_text.py
@@ -1,20 +1,26 @@
 from google.cloud import speech 
 
+#establishes client connection
 client = speech.SpeechClient.from_service_account_file('key.json')
 
+#sets file name
 file_name = "test.mp3"
 
+#reads in the mp3 data
 with open(file_name, 'rb') as i:
     mp3_data = i.read()
 
+#sets audio file and instantiates the recongition process
 audio_file = speech.RecognitionAudio(content=mp3_data)
 
+#sets speed and language for the voice
 config = speech.RecognitionConfig(
     sample_rate_hertz=44100,
     enable_automatic_punctuation = True,
     language_code = "en-US"
 )
 
+#finishes client session
 response = client.recognize(
     config = config, 
     audio = audio_file
