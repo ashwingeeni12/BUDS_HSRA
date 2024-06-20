8e4cb0e45cbd5354270781a03cbaa5b9a600f702
ashwingeeni12 <99517865+ashwingeeni12@users.noreply.github.com>
2024-06-20 14:27:49 -0500

    Update text_speech.py
    changed to a function so directions.py could access

--- text_speech.py
+++ text_speech.py
@@ -2,31 +2,32 @@ import os
 import directions
 from google.cloud import texttospeech
 
-os.environ['GOOGLE_APPLICATION_CREDENTIALS'] = 'key.json'
-client = texttospeech.TextToSpeechClient()
+def tts(direction):
+    os.environ['GOOGLE_APPLICATION_CREDENTIALS'] = 'key.json'
+    client = texttospeech.TextToSpeechClient()
 
-# text is where the google maps direction will go
-raw_text = directions.direction
+    # text is where the google maps direction will go
+    raw_text = direction
 
-synthesis_input = texttospeech.SynthesisInput(text=raw_text)
+    synthesis_input = texttospeech.SynthesisInput(text=raw_text)
 
-voice = texttospeech.VoiceSelectionParams(
-    language_code = "en-US",
-    name = "en-US-Studio-O"
-)
+    voice = texttospeech.VoiceSelectionParams(
+        language_code = "en-US",
+        name = "en-US-Studio-O"
+    )
 
-audio_config = texttospeech.AudioConfig(
-    audio_encoding = texttospeech.AudioEncoding.MP3,
-    effects_profile_id = ["small-bluetooth-speaker-class-device"],
-    speaking_rate = 1,
-    pitch = 1
-)
+    audio_config = texttospeech.AudioConfig(
+        audio_encoding = texttospeech.AudioEncoding.MP3,
+        effects_profile_id = ["small-bluetooth-speaker-class-device"],
+        speaking_rate = 1,
+        pitch = 1
+    )
 
-response = client.synthesize_speech(
-    input = synthesis_input,
-    voice = voice,
-    audio_config = audio_config
-)
+    response = client.synthesize_speech(
+        input = synthesis_input,
+        voice = voice,
+        audio_config = audio_config
+    )
 
-with open("speech.mp3", "wb") as i:
-    i.write(response.audio_content)
+    with open("speech.mp3", "wb") as i:
+        i.write(response.audio_content)
