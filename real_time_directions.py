02f5999da9224e0ae7d1984471e800309043e4d1
ashwingeeni12 <99517865+ashwingeeni12@users.noreply.github.com>
2024-06-20 16:07:15 -0500

    Update real_time_directions.py

--- real_time_directions.py
+++ real_time_directions.py
@@ -3,6 +3,8 @@ from datetime import datetime
 import re
 import html
 from geopy.distance import geodesic
+import speech_text
+from text_speech import *
 
 import time
 
@@ -11,11 +13,11 @@ apiKey = "AIzaSyBP0A4jy_5ydtdGR0qtQ14glSJkF4Jw9IM"
 map_client = googlemaps.Client(apiKey)
 
 # Takes start and end locations from user
-start = input("Enter your start location: ")
-end = input("Enter your end location: ")
+start = speech_text.origin
+end = speech_text.destination
 
 # Walking or driving directions
-mode = input("Enter your mode of transportation (driving, walking): ")
+mode = walking
 
 # This requests the directions
 directions = map_client.directions(start, end, mode=mode)
@@ -36,17 +38,16 @@ for route in directions:
     for leg in route['legs']:
         for step in leg['steps']:
             clean_instructions = re.sub('<[^<]+?>', '', html.unescape(step['html_instructions']))
-            next_turn_location = (step['end_location']['lat'], step['end_location']['lng'])
-            print("\nNext instruction: ", clean_instructions)
-            print("")
-
+            tts(clean_instructions)
             while True:
                 current_location = get_current_location()
                 distance = calculate_distance(current_location, next_turn_location)
-                print(f"Distance to next turn: {distance:.2f} meters")
+                if distance <= 42 and distance >= 40:  # If within 40 meters of the turn
+                    tts(clean_instructions + " in 40 meters)
+                if distance >= 10 and distance <= 12: 
+                    tts(clean_instructions + " in 10 meters) 
+                if distance <= 2: 
+                    tts(clean_instructions + " in 2 meters)
+                    break 
 
-                if distance < 50:  # If within 50 meters of the turn
-                    print(f"Instruction: {clean_instructions}")
-                    break
-
-                time.sleep(10)  # Check location every 10 seconds
+            
