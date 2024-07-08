9c850b04ec1774106fde940abd620405d446e398
ashwingeeni12 <99517865+ashwingeeni12@users.noreply.github.com>
2024-07-08 15:47:49 -0500

    Update gps.py

--- gps.py
+++ gps.py
@@ -2,23 +2,25 @@ import serial
 
 def location():
     location = []
-
+    gps = serial.Serial("com6", 9600)
     while True:
-        gps = serial.Serial("pi port", 9600)
         gps_raw_data = gps.readline()
         gps_data = gps_raw_data.decode("utf-8")
         gps_list = gps_data.split(",")
         if gps_list[0] == "$GPGGA":
             lat = gps_list[2]
-            latS = lat[0:lat.index('.')]
-            lat = latS[0:(len(latS) - 2)] + ' ' + latS[(len(latS) - 2):len(latS)] + "." + lat[(lat.index('.') + 1): len(lat)]
-            lon = gps_list[4]
-            lonS = lon[0:lon.index('.')]
-            lon = lonS[0:(len(lonS) - 2)] + ' ' + lonS[(len(lonS) - 2):len(lonS)] + "."  + lon[(lon.index('.') + 1): len(lon)]
-            location = [lat, lon]
-        break
+            if (lat == "") == False:
+                latS = lat[0:lat.index('.')]
+                latDD = latS[0:(len(latS) - 2)]
+                if gps_list[3] == 'S':
+                    latDD = "-" + latDD
+                lat = latDD + ' ' + latS[(len(latS) - 2):len(latS)] + "." + lat[(lat.index('.') + 1): len(lat)]
+                lon = gps_list[4]
+                lonS = lon[0:lon.index('.')]
+                lonDD = lonS[0:(len(lonS) - 2)]
+                if gps_list[5] == 'W':
+                    lonDD = "-" + lonDD
+                lon = lonDD + ' ' + lonS[(len(lonS) - 2):len(lonS)] + "."  + lon[(lon.index('.') + 1): len(lon)]
+                location = [lat, lon]
+                break
     return location
-
-def location(x):
-    list = ["30.289790, -97.741344", "30.289729, -97.740541", "30.289692, -97.740280", "30.286547, -97.738484"]
-    return list[x]
